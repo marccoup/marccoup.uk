167840db4b27f6d11e2ce3a537e075238c21ec13
Marc Coupland <marcus@vibeagency.uk>
2024-06-22 13:16:28 +0100

    Build holding page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ export default {
     content: [
         './resources/**/*.antlers.html',
         './resources/**/*.antlers.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.vue',
         './content/**/*.md',
     ],
 
