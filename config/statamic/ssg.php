e815fe40e726e17f78adb3ccb7fbc58b2e6fd795
Marc Coupland <marc@marccoup.uk>
2025-11-22 20:11:04 +0000

    wip

--- config/statamic/ssg.php
+++ config/statamic/ssg.php
@@ -41,6 +41,7 @@ return [
     'copy' => [
         public_path('build') => 'build',
         public_path('assets') => 'assets',
+        public_path('favicon') => 'favicon',§
     ],
 
     'symlinks' => [
