f057ff1f464be20770b5eb43942ac31fca60ec22
Marc Coupland <marc@marccoup.uk>
2026-04-20 07:36:41 +0100

    Add example images for photo layout testing

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/style.css',
+                'resources/js/site.js',
             ],
             refresh: true,
         }),
